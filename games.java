0f35f0a2c455a798c080119bd5f02b8d17947fda
pituzdg <pituzdg@gmail.com>
2013-10-21 20:49:39 +0200

    Create games.java

---
+++ games.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author pituzdg
+ */
+public class GAMES extends CD{
+    private String kind="";
+    private String title="";
+    
+    public String getTitle() 
+    {
+        return title;
+    }
+    public void setTitle(String title) 
+    {
+        this.title = title;
+    }
+    public String getKind()
+    {
+        return kind;
+    }
+    public void setKind(String kind)
+    {
+       this.kind = kind;
+    }
+    
+}
